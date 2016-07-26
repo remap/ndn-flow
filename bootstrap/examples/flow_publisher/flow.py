ebfc70c5fbaa2e57de5344c56cb0de6c64a19697
zhehaowang <wangzhehao410305@gmail.com>
2016-07-26 16:20:30 -0400

    examples: send publish request in Flow

--- bootstrap/examples/flow_publisher/flow.py
+++ bootstrap/examples/flow_publisher/flow.py
@@ -10,6 +10,9 @@ from pyndn.security import KeyChain, SecurityException
 from pyndn.security.identity import IdentityManager, FilePrivateKeyStorage, BasicIdentityStorage
 from pyndn.security.policy import NoVerifyPolicyManager
 from pyndn.threadsafe_face import ThreadsafeFace
+from pyndn.encoding import ProtobufTlv
+
+from ndn_pi.commands import AppRequestMessage
 
 import time
 import sys
@@ -24,23 +27,25 @@ except ImportError:
 
 class FlowPublisher(object):
     def __init__(self, face, keyChain, loop, confFile = "flow.conf"):
-        self._keyChain = keyChain
         self._defaultIdentity = None
         self._dataPrefix = None
-        self._loop = loop
+        self._defaultCertificateName = None
+        self._controllerName = None
 
+        self._keyChain = keyChain
+        self._loop = loop
         self._face = face
 
         self.processConfiguration(confFile)
-        self._defaultCertificateName = self._keyChain.getIdentityManager().getDefaultCertificateNameForIdentity(self._defaultIdentity)
-
+        
         self._face.setCommandSigningInfo(self._keyChain, self._defaultCertificateName)
         print "Using default certificate name: " + self._defaultCertificateName.toUri()
 
-    def processConfiguration(self, confFile):
+    def processConfiguration(self, confFile, requestPermission = True):
         with open(confFile, "r") as f:
             conf = f.read()
             confObj = json.loads(conf)
+            
             if "identity" in confObj:
                 if confObj["identity"] == "default":
                     self._defaultIdentity = self._keyChain.getDefaultIdentity()
@@ -51,10 +56,74 @@ class FlowPublisher(object):
                         self._defaultIdentity = defaultIdName
                     except SecurityException:
                         print "Identity " + defaultIdName.toUri() + " in configuration file " + confFile + " does not exist. Please configure the device with this identity first"
+            else:
+                self._defaultIdentity = self._keyChain.getDefaultIdentity()
+            # TODO: handling the case where no default identity is present
+            self._defaultCertificateName = self._keyChain.getIdentityManager().getDefaultCertificateNameForIdentity(self._defaultIdentity)
+
             if "prefix" in confObj:
                 self._dataPrefix = Name(confObj["prefix"])
             else:
                 print "Configuration file " + confFile + " is missing application data prefix"
+            
+            # TODO: handling signature with direct bits instead of keylocator keyname
+            signerName = self._keyChain.getCertificate(self._defaultCertificateName).getSignature().getKeyLocator().getKeyName()    
+            if "signer" in confObj:    
+                if confObj["signer"] == "default":
+                    print "Deriving from " + signerName.toUri() + " for controller name"
+                else:
+                    intendedSigner = confObj["signer"]
+                    if intendedSigner != signerName.toUri():
+                        print "Configuration file " + confFile + " signer names mismatch: expected " + intendedSigner + "; got " + signerName.toUri()
+                    else:
+                        signerName = Name(intendedSigner)
+            else:
+                print "Deriving from " + signerName.toUri() + " for controller name"
+            self._controllerName = self.getIdentityNameFromCertName(signerName)
+            print "Controller name: " + self._controllerName.toUri()
+
+        if requestPermission:
+            self.sendAppRequest()
+        return
+
+    def getIdentityNameFromCertName(self, certName):
+        i = certName.size() - 1
+
+        idString = "KEY"
+        while i >= 0:
+            if certName.get(i).toEscapedString() == idString:
+                break
+            i -= 1
+
+        if i < 0:
+            print "Error: unexpected certName " + certName.toUri()
+            return None
+
+        return certName.getPrefix(i)
+
+    def sendAppRequest(self):
+        message = AppRequestMessage()
+
+        for component in range(self._defaultIdentity.size()):
+            message.command.idName.components.append(self._defaultIdentity.get(component).toEscapedString())
+        for component in range(self._dataPrefix.size()):
+            message.command.dataPrefix.components.append(self._dataPrefix.get(component).toEscapedString())
+        paramComponent = ProtobufTlv.encode(message)
+
+        requestInterest = Interest(Name(self._controllerName).append("requests").append(paramComponent))
+        # TODO: change this. (for now, make this request long lived (100s), if the controller operator took some time to respond)
+        requestInterest.setInterestLifetimeMilliseconds(100000)
+        self._keyChain.sign(requestInterest, self._defaultCertificateName)
+        self._face.expressInterest(requestInterest, self.onAppRequestData, self.onAppRequestTimeout)
+        print "Application publish request sent: " + requestInterest.getName().toUri()
+        return
+
+    def onAppRequestData(self, interest, data):
+        print "Got application publishing request data"
+        return
+
+    def onAppRequestTimeout(self, interest):
+        print "Application publishing request times out"
         return
     
     def start(self):
