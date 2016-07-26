585c66b0c29b28f8ab2b862d1d5cdc98c912c51f
zhehaowang <wangzhehao410305@gmail.com>
2016-07-26 18:01:08 -0400

    iot-cpp: add boost dependency for threadsafe-face

--- bootstrap/ndn_iot_cpp/include/base-node.hpp
+++ bootstrap/ndn_iot_cpp/include/base-node.hpp
@@ -4,7 +4,7 @@
 #include <ndn-cpp/name.hpp>
 #include <ndn-cpp/interest.hpp>
 #include <ndn-cpp/data.hpp>
-#include <ndn-cpp/face.hpp>
+#include <ndn-cpp/threadsafe-face.hpp>
 
 #include <ndn-cpp/security/key-chain.hpp>
 #include <ndn-cpp/security/identity/identity-manager.hpp>
@@ -13,6 +13,7 @@
 #include <ndn-cpp/security/policy/config-policy-manager.hpp>
 
 #include "common.hpp"
+#include <boost/asio.hpp>
 
 namespace ndn_iot {
 
@@ -25,6 +26,11 @@ private:
   ndn::BasicIdentityStorage identityStorage;
   ndn::ConfigPolicyManager policyManager;
   ndn::IdentityManager identityManager;
+  
+  boost::asio::io_service ioService;
+  ndn::ptr_lib::shared_ptr<ndn::ThreadsafeFace> face;
+
+  bool setupComplete;
 };
 
 }
--- bootstrap/ndn_iot_cpp/src/base-node.cpp
+++ bootstrap/ndn_iot_cpp/src/base-node.cpp
@@ -1,9 +1,12 @@
 #include "base-node.hpp"
 
 using namespace ndn_iot;
+using namespace ndn;
+using namespace std;
+using namespace ndn::func_lib;
 
 BaseNode::BaseNode() {
-
+  
 }
 
 BaseNode::~BaseNode() {
