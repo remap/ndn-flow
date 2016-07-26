0399eee933770c02a374f746249d96554c262f88
zhehaowang <wangzhehao410305@gmail.com>
2016-07-26 13:22:47 -0400

    examples: publish psutil data in flow example

--- bootstrap/examples/add_device/publisher.py
+++ bootstrap/examples/add_device/publisher.py
@@ -38,9 +38,6 @@ class CachedContentPublisher(IotNode):
         d.getMetaInfo().setFreshnessPeriod(10000)
         return d
 
-    def registerCachePrefix(self):
-        self._dataCache.registerPrefix(self._dataPrefix, self.cacheRegisterFail , self.onDataMissing)
-
     def unknownCommandResponse(self, interest):
         # we override this so the MemoryContentCache can handle data requests
         afterPrefix = interest.getName().get(self.prefix.size()).toEscapedString()
@@ -48,36 +45,6 @@ class CachedContentPublisher(IotNode):
             return None
         else:
             return super(CachedContentPublisher, self).unknownCommandResponse(interest)
-
-    def cacheRegisterFail(self, interest):
-        # just try again
-        self.log.warn('Could not register data cache')
-        self.registerCachePrefix()
-
-    def onDataMissing(self, prefix, interest, transport, prefixId):
-        self._missedRequests += 1
-        # let it timeout
-
-    def publishData(self):
-        timestamp = time.time() 
-        cpu_use = ps.cpu_percent()
-        users = [u.name for u in ps.users()]
-        nProcesses = len(ps.pids())
-        memUse = ps.virtual_memory().percent
-        swapUse = ps.swap_memory().percent
-
-        info = {'cpu_usage':cpu_use, 'users':users, 'processes':nProcesses,
-                 'memory_usage':memUse, 'swap_usage':swapUse}
-    
-        dataOut = Data(Name(self._dataPrefix).appendVersion(int(timestamp)))
-        dataOut.setContent(json.dumps(info))
-        dataOut.getMetaInfo().setFreshnessPeriod(10000)
-        self.signData(dataOut)
-
-        self._dataCache.add(dataOut)
-
-        # repeat every 5 seconds
-        self.loop.call_later(5, self.publishData)
 
 if __name__ == '__main__':
     try:
--- bootstrap/examples/flow_publisher/flow.py
+++ bootstrap/examples/flow_publisher/flow.py
@@ -4,11 +4,12 @@
 # This is the data publisher using MemoryContentCache
 #
 
-from pyndn import Name, Face, Interest, Data
+from pyndn import Name, Interest, Data
 from pyndn.util.memory_content_cache import MemoryContentCache
 from pyndn.security import KeyChain, SecurityException
 from pyndn.security.identity import IdentityManager, FilePrivateKeyStorage, BasicIdentityStorage
 from pyndn.security.policy import NoVerifyPolicyManager
+from pyndn.threadsafe_face import ThreadsafeFace
 
 import time
 import sys
@@ -16,10 +17,18 @@ import json
 
 import logging
 
+try:
+    import asyncio
+except ImportError:
+    import trollius as asyncio
+
 class FlowPublisher(object):
-    def __init__(self, face, keyChain, confFile = "flow.conf"):
+    def __init__(self, face, keyChain, loop, confFile = "flow.conf"):
         self._keyChain = keyChain
         self._defaultIdentity = None
+        self._dataPrefix = None
+        self._loop = loop
+
         self._face = face
 
         self.processConfiguration(confFile)
@@ -42,10 +51,51 @@ class FlowPublisher(object):
                         self._defaultIdentity = defaultIdName
                     except SecurityException:
                         print "Identity " + defaultIdName.toUri() + " in configuration file " + confFile + " does not exist. Please configure the device with this identity first"
+            if "prefix" in confObj:
+                self._dataPrefix = Name(confObj["prefix"])
+            else:
+                print "Configuration file " + confFile + " is missing application data prefix"
         return
     
     def start(self):
+        self._dataCache = MemoryContentCache(self._face, 100000)
+        self.registerCachePrefix()
+        print "Serving data at {}".format(self._dataPrefix.toUri())
+        self._loop.call_soon(self.publishData)
         return
+
+    def registerCachePrefix(self):
+        self._dataCache.registerPrefix(self._dataPrefix, self.cacheRegisterFail, self.onDataMissing)
+
+    def cacheRegisterFail(self, interest):
+        # just try again
+        self.log.warn('Could not register data cache')
+        self.registerCachePrefix()
+
+    def onDataMissing(self, prefix, interest, transport, prefixId):
+        self._missedRequests += 1
+        # let it timeout
+
+    def publishData(self):
+        timestamp = time.time() 
+        cpu_use = ps.cpu_percent()
+        users = [u.name for u in ps.users()]
+        nProcesses = len(ps.pids())
+        memUse = ps.virtual_memory().percent
+        swapUse = ps.swap_memory().percent
+
+        info = {'cpu_usage':cpu_use, 'users':users, 'processes':nProcesses,
+                 'memory_usage':memUse, 'swap_usage':swapUse}
+    
+        dataOut = Data(Name(self._dataPrefix).appendVersion(int(timestamp)))
+        dataOut.setContent(json.dumps(info))
+        dataOut.getMetaInfo().setFreshnessPeriod(10000)
+        self._keyChain.sign(dataOut, self._defaultCertificateName)
+
+        self._dataCache.add(dataOut)
+
+        # repeat every 5 seconds
+        self._loop.call_later(5, self.publishData)
 
 if __name__ == '__main__':
     try:
@@ -53,8 +103,12 @@ if __name__ == '__main__':
     except Exception as e:
         print str(e)
     identityManager = IdentityManager(BasicIdentityStorage(), FilePrivateKeyStorage())
-    face = Face()
+    
+    loop = asyncio.get_event_loop()
+    face = ThreadsafeFace(loop)
     keyChain = KeyChain()
 
-    n = FlowPublisher(face, keyChain)
+    n = FlowPublisher(face, keyChain, loop)
     n.start()
+
+    loop.run_forever()
