3a96948d961bc3fa4c5f88d00d2fc18fbdb132a7
zhehaowang <wangzhehao410305@gmail.com>
2016-08-02 19:35:04 -0400

    example-call: remove duplicated file

--- design/app_call_example.py
+++ design/app_call_example.py
@@ -10,44 +10,43 @@ Covered functions include:
 class IotApplication():
     def __init__(self):
         # Initialize face
-        self.loop = asyncio.get_event_loop()
         self.face = ThreadsafeFace(self.loop)
 
     def start(self):
         # Creates an instance of Bootstrap class, which bootstraps keyChain, default identity and certificate name (for publishing), and policy manager (for consuming)
         # This application does both publishing and consuming, so we call setupKeyChain and startTrustSchemaUpdate
         bootstrap = Bootstrap(self.face)
-        bootstrap.setupKeyChain("app.conf", onSetupComplete = self.onSetupComplete, onSetupFailed = self.onSetupFailed)
-        bootstrap.startTrustSchemaUpdate(self, namespace, onUpdateSuccess = self.onUpdateSuccess, onUpdateFailed = self.onUpdateFailed)
+        bootstrap.setupDefaultIdentityAndRoot("app.conf", onSetupComplete, onSetupFailed)
 
-        self.loop.run_forever()
+    def onSetupComplete(defaultIdentity, keyChain):
+        # KeyChain setup successul
+        # Requests production authorization (trust schema update and distribution by controller)
+        bootstrap.requestProducerAuthorization(applicationProducingNamespace, appName, deviceIdentity, onRequestSuccess, onRequestFailed)
+        # Requests trust schema so that we can authenticate consumed messages
+        bootstrap.startTrustSchemaUpdate(Name(applicationTrustSchemaNamespace), self.onUpdateSuccess, self.onUpdateFailed)    
 
-    def onSetupComplete(self, defaultIdentity, keyChain):
-        # KeyChain setup successul, this application can now publish, and run discovery (which requires publishing functions to be set up)
-        producer = AppProducerMemoryContentCache(appProducePrefix)
+    def onRequestSuccess(self):
+        # Producing permission granted; this application can now publish, and run discovery (which requires publishing functions to be set up)
+        producer = AppProducerMemoryContentCache(self.face, self.keyChain, self.defaultCertificateName, applicationProducingNamespace)
         producer.produce(someSuffix, content)
-        discovery = Discovery()
+        
+        discovery = Discovery(self.face, self.keyChain, self.defaultCertificateName, discoveryNamespace)
         discovery.start()
-
-    def onSetupFailed(self, msg):
         return
 
-    def onUpdateSuccess(self, initialUpdate):
+    def onUpdateSuccess(self, trustSchemaString, isInitial):
         # Initial trust schema is received and verified 
         # (to verify initial trust schema, the bootstrap instance loads a default trust schema with trust anchor cert, written by the add_device process)
         # all later contents can be verified so we start consuming
         if initialUpdate:
-            consumer = SequenceNumberConsumer(appConsumePrefix)
+            consumer = SequenceNumberConsumer(self.face, self.keyChain, self.defaultCertificateName, applicationConsumingNamespace)
             consumer.consume(someSuffix, onComplete, onFailed)
 
-    def onUpdateFailed(self, msg):
-        return
-
 if __name__ == '__main__':
-    try:
-        import psutil as ps
-    except Exception as e:
-        print str(e)
-    myIotApplication = IotApplication()
+    loop = asyncio.get_event_loop()
+    face = ThreadsafeFace(loop)
+    
+    myIotApplication = IotApplication(face)
+    myIotApplication.start()
 
     loop.run_forever()
