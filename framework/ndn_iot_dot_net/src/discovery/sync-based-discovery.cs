a92a07c67c9900ad3250907459abe7a7f3419b34
zhehaowang <wangzhehao410305@gmail.com>
2016-09-22 19:51:07 -0700

    ndn-iot-dot-net: add stub for discovery

---
+++ framework/ndn_iot_dot_net/src/discovery/sync-based-discovery.cs
@@ -0,0 +1,10 @@
+namespace ndn_iot.discovery {
+    using System;
+    
+    using net.named_data.jndn;
+    using net.named_data.jndn.security;
+
+    public class SyncBasedDiscovery {
+    	public SyncBasedDiscovery() {}
+    }
+}
