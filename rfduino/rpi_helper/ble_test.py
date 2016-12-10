262b8e38588f2d2731d650eb9b61d73e3edf2061
zhehaowang <wangzhehao410305@gmail.com>
2016-12-10 01:56:02 +0000

    rpi-helper: skeleton for btle connection

--- rfduino/rpi_helper/ble_test.py
+++ rfduino/rpi_helper/ble_test.py
@@ -2,17 +2,21 @@ import binascii
 import time
 from bluepy.btle import UUID, Peripheral
 
+from pyndn.encoding.element_reader import ElementReader
+
 my_uuid = UUID(0x2221)
 p = Peripheral("EE:C5:46:65:D3:C1", "random")
+
+el = ElementListener
+em = ElementReader(el)
 
 try:
     ch = p.getCharacteristics(uuid=my_uuid)[0]
     if (ch.supportsRead()):
         while True:
-            print ch.read()
-            val = binascii.b2a_hex(ch.read())
-            val = binascii.unhexlify(val)
-            print "got " + val
-            time.sleep(1)
+            val = ch.read()
+            print len(val)
+            em.onReceivedData(val)
+            time.sleep(0.001)
 finally:
     p.disconnect()
