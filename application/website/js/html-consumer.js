08ab4e3f75b08c80a81a4dfd594a35bc54ff6bf7
zhehaowang <wangzhehao410305@gmail.com>
2016-12-28 18:45:18 -0800

    browser: in html-consumer add option to fetch repeatedly; add modified SegmentFetcher

--- application/website/js/html-consumer.js
+++ application/website/js/html-consumer.js
@@ -10,17 +10,45 @@ var HtmlConsumer = function HtmlConsumer(prefix, mobileId, face, keyChain) {
     this.face = face;
     this.keyChain = keyChain;
     this.baseInterest = new Interest(new Name(prefix).append(HTML_FETCH_COMMAND_VERB).append(new Name(this.mobileId).toUri()));
-    this.baseInterest.setInterestLifetimeMilliseconds(60000);
+    this.baseInterest.setInterestLifetimeMilliseconds(4000);
 }
 
-HtmlConsumer.prototype.consume = function(onComplete) {
-    console.log(this.baseInterest.toUri());
-    SegmentFetcher.fetch
+HtmlConsumer.prototype.consume = function(onComplete, repeatWithExclusion) {
+    var self = this;
+    var repeat = repeatWithExclusion;
+    if (repeat === undefined) {
+        repeat = true;
+    }
+    console.log("fetch: " + this.baseInterest.getName().toUri());
+    console.log("exclude: " + this.baseInterest.getExclude().toUri());
+    SegmentFetcherDataName.fetch
       (this.face, this.baseInterest, null,
-        function(content) {
-            onComplete(content);
+        function (dataName, content) {
+            if (repeat) {
+                self.completeAndExpress(onComplete, dataName, content);
+            } else {
+                onComplete(content);
+            }
         },
-        function(errorCode, message) {
+        function (errorCode, message) {
             console.log(errorCode + " : " + message);
+            if (repeat) {
+                console.log("retrying");
+                self.timeoutAndRetry(onComplete);
+            }
         });
+}
+
+HtmlConsumer.prototype.completeAndExpress = function (onComplete, dataName, content) {
+    onComplete(content);
+    var versionComponent = dataName.get(this.baseInterest.getName().size());
+    var exclude = new Exclude();
+    exclude.appendAny();
+    exclude.appendComponent(versionComponent);
+    this.baseInterest.setExclude(exclude);
+    this.consume(onComplete, true);
+}
+
+HtmlConsumer.prototype.timeoutAndRetry = function (onComplete) {
+    this.consume(onComplete, true);
 }
